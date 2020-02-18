d51aec38cd9dc012d8a4db62b6f2eafac8753a58
Baptiste Hector <baptiste.hector@caliatys.com>
2020-02-18 13:58:06 +0100

    Friend Dialog

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { User, Contact } from '../models';
 import { AuthenticationService, UserService, AlertService } from '../services';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ReplaySubject, Subscription, Subject } from 'rxjs';
+import { takeUntil, first, take } from 'rxjs/operators';
+import { MatSelect } from '@angular/material/select';
 
 @Component({
   selector: 'app-profile',
@@ -86,6 +89,117 @@ export class ContactDialog {
       error => {
           this.alertService.error(error);
       });
+    }
+
+}
+
+@Component({
+  templateUrl: 'friend-dialog.html',
+  styleUrls: ['./profile.component.css'],
+})
+export class FriendDialog {
+  currentUser: User
+  friendForm: FormGroup;
+  public filteredUsersMulti: ReplaySubject<User[]> = new ReplaySubject<User[]>(1);
+  tmpUsers = new Array();
+  protected _onDestroy = new Subject<void>();
+    private inputWatcher: Subscription;
+
+  @ViewChild('singleSelect', { static: true }) singleSelect: MatSelect;
+  @ViewChild('multiSelect', { static: true }) multiSelect: MatSelect;
+  constructor(
+      private authenticationService: AuthenticationService,
+      private formBuilder: FormBuilder,
+      private userService: UserService,
+      private alertService: AlertService,
+      public dialogRef: MatDialogRef<FriendDialog>,
+      @Inject(MAT_DIALOG_DATA) public data: User)
+      {
+          this.currentUser = this.authenticationService.currentUserValue;          
+          this.friendForm = this.formBuilder.group({
+            users: [],
+            userMultiFilterCtrl: '',
+          });
+      }
+
+  
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    if (this.inputWatcher) {
+        this.inputWatcher.unsubscribe();
+    }
+    this._onDestroy.next();
+    this._onDestroy.complete();
+}
+
+private loadAllUsers() {
+  this.userService.getUsers()
+      .pipe(first())
+      .subscribe(users => {
+          this.tmpUsers = users
+          this.tmpUsers = users.filter(user => user.id != this.currentUser.id)
+          console.log(this.tmpUsers);
+          this.filteredUsersMulti.next(this.tmpUsers.slice());
+      });
+}
+
+protected setInitialValue() {
+  this.filteredUsersMulti
+    .pipe(take(1), takeUntil(this._onDestroy))
+    .subscribe(() => {
+      this.multiSelect.compareWith = (a: User, b: User) => a && b && a.id === b.id;
+    });
+}
+
+protected filterUsersMulti() {
+
+  // get the search keyword
+  let search = this.friendForm.controls.userMultiFilterCtrl.value;
+  if (!search) {
+    this.filteredUsersMulti.next(this.tmpUsers.slice());
+    return;
+  } else {
+    search = search.toLowerCase();
+  }
+  // filter the banks
+  this.filteredUsersMulti.next(
+    this.tmpUsers.filter(user => user.username.toLowerCase().indexOf(search) > -1)
+  );
+}
+
+  ngAfterViewInit() {
+    this.friendForm.controls.userMultiFilterCtrl.valueChanges
+        .pipe(takeUntil(this._onDestroy))
+        .subscribe(() => {
+            this.filterUsersMulti();
+        });
+  }
+
+  public onSubmit() {
+    let user_id: string = '';
+        let users: User[] = this.friendForm.controls.users.value;
+        if (users){
+        users.forEach(element => {
+            user_id += element.id.toString() + ',';
+        });
+        }
+        this.currentUser.friend_id = user_id
+        this.userService.updateUser(this.currentUser)
+        .subscribe (
+          data => {
+            this.alertService.success('Friend added', true);
+            this.dialogRef.close();
+          },
+          error => {
+            this.alertService.error(error);
+        }
+        )
     }
 
 }
